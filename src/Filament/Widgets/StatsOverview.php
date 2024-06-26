607c76f591fb9d3b92cf3906e447eda263a23f8c
Dasun Tharanga <hello@dasun.dev>
2024-06-26 23:42:38 +0530

    wip

--- src/Filament/Widgets/StatsOverview.php
+++ src/Filament/Widgets/StatsOverview.php
@@ -17,16 +17,16 @@ class StatsOverview extends BaseWidget
                 ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('success'),
-            Stat::make('Refunds', Payment::refunded()->count())
-                ->description('10k increase')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->color('danger'),
             Stat::make('Subscriptions', Subscription::count())
                 ->description('10k increase')
                 ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->color('info'),
+            Stat::make('Refunds', Payment::refunded()->count())
+                ->description('10k increase')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('danger'),
         ];
     }
 }
