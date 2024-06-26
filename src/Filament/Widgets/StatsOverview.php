d6bb4664ef81472022234c74f0b0c593fed2b2e0
Dasun Tharanga <hello@dasun.dev>
2024-06-26 22:50:43 +0530

    wip

--- src/Filament/Widgets/StatsOverview.php
+++ src/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,8 @@
 
 namespace Dasundev\PayHere\Filament\Widgets;
 
+use Dasundev\PayHere\Models\Payment;
+use Dasundev\PayHere\Models\Subscription;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,9 +12,21 @@ class StatsOverview extends BaseWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total payments', '192.1k'),
-            Stat::make('Refund payments', '21%'),
-            Stat::make('Profit', '3:12'),
+            Stat::make('Payments', Payment::count())
+                ->description('10k increase')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('success'),
+            Stat::make('Refunds', Payment::refunded()->count())
+                ->description('10k increase')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('danger'),
+            Stat::make('Subscriptions', Subscription::count())
+                ->description('10k increase')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->color('info'),
         ];
     }
 }
