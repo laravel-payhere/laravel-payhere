dad067e2011fdba84184757a1bc5d884343bceb9
Dasun Tharanga <hello@dasun.dev>
2024-06-26 20:43:21 +0530

    style: pint fixes

--- src/Enums/PaymentMethod.php
+++ src/Enums/PaymentMethod.php
@@ -7,14 +7,14 @@ namespace Dasundev\PayHere\Enums;
  */
 enum PaymentMethod: string
 {
-    case VISA = "VISA";
-    case MASTER = "MASTER";
-    case AMEX = "AMEX";
-    case EZCASH = "EZCASH";
-    case MCASH = "MCASH";
-    case GENIE = "GENIE";
-    case VISHWA = "VISHWA";
-    case PAYAPP = "PAYAPP";
-    case HNB = "HNB";
-    case FRIMI = "FRIMI";
+    case VISA = 'VISA';
+    case MASTER = 'MASTER';
+    case AMEX = 'AMEX';
+    case EZCASH = 'EZCASH';
+    case MCASH = 'MCASH';
+    case GENIE = 'GENIE';
+    case VISHWA = 'VISHWA';
+    case PAYAPP = 'PAYAPP';
+    case HNB = 'HNB';
+    case FRIMI = 'FRIMI';
 }
