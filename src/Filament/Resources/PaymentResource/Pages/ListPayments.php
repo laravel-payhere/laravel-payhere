9ef04a792786747935b2906e47c6f6d7dd8e2cb6
Dasun Tharanga <hello@dasun.dev>
2024-06-26 20:25:56 +0530

    feat(panel): delete create action

--- src/Filament/Resources/PaymentResource/Pages/ListPayments.php
+++ src/Filament/Resources/PaymentResource/Pages/ListPayments.php
@@ -4,16 +4,8 @@ namespace Dasundev\PayHere\Filament\Resources\PaymentResource\Pages;
 
 use Dasundev\PayHere\Filament\Resources\PaymentResource\PaymentResource;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Actions;
 
 class ListPayments extends ListRecords
 {
     protected static string $resource = PaymentResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            Actions\CreateAction::make(),
-        ];
-    }
 }
