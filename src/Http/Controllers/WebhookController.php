acefa2a1962a7a62aff365637f6e98a991305432
Dasun Tharanga <hello@dasun.dev>
2024-06-30 15:33:09 +0530

    wip

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -10,6 +10,7 @@ use Dasundev\PayHere\Models\Payment;
 use Dasundev\PayHere\PayHere;
 use Dasundev\PayHere\Repositories\PaymentRepository;
 use Dasundev\PayHere\Repositories\SubscriptionRepository;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Log;
@@ -65,7 +66,7 @@ class WebhookController extends Controller
 
         $user = $order->{$relationship};
 
-        $payment = $this->paymentRepository->createPayment($user, $request);
+        $payment = $this->createPayment($user, $request);
 
         event(new PaymentVerified($payment));
 
@@ -80,5 +81,37 @@ class WebhookController extends Controller
     {
         return (int) $request->recurring === 1
             && (int) $request->item_rec_status === SubscriptionStatus::Active->value;
+    }
+
+    private function createPayment($user, Request $request): Payment
+    {
+        return Payment::create([
+            'user_id' => $user?->id,
+            'merchant_id' => $request->merchant_id,
+            'order_id' => $request->order_id,
+            'payment_id' => $request->payment_id,
+            'authorization_token' => $request->authorization_token,
+            'subscription_id' => $request->subscription_id,
+            'payhere_amount' => $request->payhere_amount,
+            'captured_amount' => $request->captured_amount,
+            'payhere_currency' => $request->payhere_currency,
+            'status_code' => $request->status_code,
+            'md5sig' => $request->md5sig,
+            'status_message' => $request->status_message,
+            'method' => $request->input('method'),
+            'card_holder_name' => $request->card_holder_name,
+            'card_no' => $request->card_no,
+            'card_expiry' => $request->card_expiry,
+            'recurring' => $request->recurring,
+            'message_type' => $request->message_type,
+            'item_recurrence' => $request->item_recurrence,
+            'item_duration' => $request->item_duration,
+            'item_rec_status' => $request->item_rec_status,
+            'item_rec_date_next' => $request->item_rec_date_next,
+            'item_rec_install_paid' => $request->item_rec_install_paid,
+            'customer_token' => $request->customer_token,
+            'custom_1' => $request->custom_1,
+            'custom_2' => $request->custom_2,
+        ]);
     }
 }
