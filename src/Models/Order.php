0884d925b01d17dad3b83b678e640d504d82d2a9
Dasun Tharanga <hello@dasun.dev>
2024-08-25 09:47:19 +0530

    wip

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -5,6 +5,9 @@ namespace LaravelPayHere\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use LaravelPayHere\Models\Contracts\PayHereOrder;
 use LaravelPayHere\PayHere;
 
@@ -20,6 +23,11 @@ class Order extends Model implements PayHereOrder
     public function lines(): HasMany
     {
         return $this->hasMany(OrderLine::class);
+    }
+
+    public function payment(): HasOne
+    {
+        return $this->hasOne(Payment::class);
     }
 
     public function payHereOrderId(): string
