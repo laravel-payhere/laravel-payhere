163e6bc5ffaee4cee0fb4c08ad0f59ff5d3eb50f
Dasun Tharanga <hello@dasun.dev>
2024-08-25 09:40:29 +0530

    wip

---
+++ src/Models/Order.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaravelPayHere\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use LaravelPayHere\Models\Contracts\PayHereOrder;
+use LaravelPayHere\PayHere;
+
+class Order extends Model implements PayHereOrder
+{
+    protected $guarded = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(PayHere::$customerModel);
+    }
+    
+    public function payHereOrderId(): string
+    {
+        return $this->id;
+    }
+
+    public function payHereOrderTotal(): float
+    {
+        return $this->total;
+    }
+}
