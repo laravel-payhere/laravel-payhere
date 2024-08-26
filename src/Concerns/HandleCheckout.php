aefcd8e38dbbb6ee690b9edfa97bc6781a6cc005
Dasun Tharanga <hello@dasun.dev>
2024-08-26 16:44:12 +0530

    wip

--- src/Concerns/HandleCheckout.php
+++ src/Concerns/HandleCheckout.php
@@ -26,6 +26,9 @@ trait HandleCheckout
 
     /**
      * Initiate the checkout process.
+     *
+     * @param  float  $amount
+     * @return \Illuminate\Contracts\View\View
      */
     public function checkout(float $amount): View
     {
