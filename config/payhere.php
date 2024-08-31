b0e2676312b553d55ddfa1b8eec8db2e868c20c4
Dasun Tharanga <hello@dasun.dev>
2024-08-31 12:37:27 +0530

    wip

--- config/payhere.php
+++ config/payhere.php
@@ -15,21 +15,6 @@ return [
     */
 
     'sandbox' => env('PAYHERE_SANDBOX', false),
-    
-//    /*
-//    |--------------------------------------------------------------------------
-//    | PayHere API Base URL
-//    |--------------------------------------------------------------------------
-//    |
-//    | This API Base URL is important for connecting to the PayHere API. By default,
-//    | it's set to the sandbox URL. Make sure to switch to the live URL when your
-//    | application goes live.
-//    |
-//    | Live URL:    https://www.payhere.lk
-//    | Sandbox URL: https://sandbox.payhere.lk
-//    */
-//
-//    'base_url' => env('PAYHERE_BASE_URL', 'https://www.payhere.lk'),
 
     /*
     |--------------------------------------------------------------------------
