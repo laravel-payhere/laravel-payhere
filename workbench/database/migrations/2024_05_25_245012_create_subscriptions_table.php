c2c3ab338531d846b3e69f5e6df93860810b4d20
Dasun Tharanga <hello@dasun.dev>
2024-09-05 20:01:14 +0530

    wip

--- workbench/database/migrations/2024_05_25_245012_create_subscriptions_table.php
+++ workbench/database/migrations/2024_05_25_245012_create_subscriptions_table.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('payhere_subscription_id')->unique()->nullable();
+            $table->string('payhere_subscription_id')->unique()->nullable();
             $table->foreignId('user_id')->nullable();
             $table->foreignId('order_id')->unique();
             $table->timestamp('trial_ends_at')->nullable();
