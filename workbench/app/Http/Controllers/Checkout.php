89f170de58c7e2b1bf5ae66c264210be83b72e95
Dasun Tharanga <hello@dasun.dev>
2024-09-08 13:39:48 +0530

    wip

--- workbench/app/Http/Controllers/Checkout.php
+++ workbench/app/Http/Controllers/Checkout.php
@@ -6,6 +6,7 @@ namespace Workbench\App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use PayHere\PayHere;
 use Workbench\App\Models\Order;
 use Workbench\App\Models\OrderLine;
 
@@ -13,13 +14,8 @@ class Checkout extends Controller
 {
     public function __invoke(Request $request)
     {
-        $order = Order::factory()
-            ->has(OrderLine::factory()->count(2), 'lines')
-            ->create();
-
-        return $request
-            ->user()
-            ->newOrder($order)
+        return PayHere::builder()
+            ->amount(100)
             ->checkout();
     }
 }
--- workbench/app/Http/Controllers/Recurring.php
+++ workbench/app/Http/Controllers/Recurring.php
@@ -6,6 +6,7 @@ namespace Workbench\App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use PayHere\PayHere;
 use Workbench\App\Models\Order;
 use Workbench\App\Models\OrderLine;
 
@@ -13,17 +14,12 @@ class Recurring extends Controller
 {
     public function __invoke(Request $request)
     {
-        $order = Order::factory()
-            ->has(OrderLine::factory()->count(2), 'lines')
-            ->create();
-
-        return $request
-            ->user()
-            ->newOrder($order)
+        return PayHere::builder()
             ->recurring(
                 recurrence: '1 Month',
                 duration: '1 Year'
             )
+            ->amount(100)
             ->checkout();
     }
 }
