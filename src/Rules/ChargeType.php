f6714f79fb07d1389234df22735a0278c4332f12
Dasun Tharanga <hello@dasun.dev>
2024-09-10 16:24:03 +0530

    wip

--- src/Rules/ChargeType.php
+++ src/Rules/ChargeType.php
@@ -6,16 +6,14 @@ namespace PayHere\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
-use PayHere\Enums\ChargeType as ChargeTypeEnum;
 
 class ChargeType implements ValidationRule
 {
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (! in_array($value, [
-            ChargeTypeEnum::Payment->value,
-            ChargeTypeEnum::Authorize->value,
-        ], true)) {
+        $chargeTypes = ['PAYMENT', 'AUTHORIZE'];
+        
+        if (! in_array($value, $chargeTypes, true)) {
             $fail('The :attribute must be either PAYMENT or AUTHORIZE.');
         }
     }
