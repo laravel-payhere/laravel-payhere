ce5b15a92e64afa22ae5831f07a935733708e4ba
Dasun Tharanga <hello@dasun.dev>
2024-09-12 15:41:49 +0530

    wip

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,167 +1,67 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ __('Redirecting to PayHere...') }}</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    
+    <title>{{ __('Checkout') }}</title>
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Figtree:400&display=swap" rel="stylesheet">
+    <link rel="dns-prefetch" href="//fonts.bunny.net" />
+    <link href="https://fonts.bunny.net/css?family=ubuntu:400,500,700" rel="stylesheet" />
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600" rel="stylesheet" />
 
-    <style>
-        :root {
-            --font-family: 'Figtree', sans-serif;
-            --background-color: #ffffff;
-            --text-color: #000000;
-            --accent-color: #6b7280;
-            --progress-bg-color: #deecff;
-            --progress-value-color: #2A41EF;
-        }
-
-        @media (prefers-color-scheme: dark) {
-            :root {
-                --background-color: #111827;
-                --text-color: #e5e7eb;
-                --progress-bg-color: #283487;
-                --progress-value-color: #5978fb;
+    <script src="https://cdn.tailwindcss.com"></script>
+    
+    <script>
+        tailwind.config = {
+            theme: {
+                fontFamily: {
+                    sans: ["Figtree", "sans-serif"],
+                },
+                extend: {
+                    colors: {
+                        blue: {
+                            50: "#f0f5fe",
+                            100: "#dce8fd",
+                            200: "#c1d7fc",
+                            300: "#97bff9",
+                            400: "#659df5",
+                            500: "#4179f0",
+                            600: "#2c59e4",
+                            700: "#2447d6",
+                            800: "#233aaa",
+                            900: "#223586",
+                            950: "#192252",
+                        },
+                        yellow: {
+                            50: "#fffdea",
+                            100: "#fff7c5",
+                            200: "#fff085",
+                            300: "#ffe146",
+                            400: "#ffcf1b",
+                            500: "#fcac00",
+                            600: "#e28400",
+                            700: "#bb5c02",
+                            800: "#984708",
+                            900: "#7c3a0b",
+                            950: "#481d00",
+                        },
+                        black: "#09090b",
+                    },
+                },
             }
         }
-
-        html, body {
-            font-family: var(--font-family);
-            height: 100vh;
-            margin: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background: var(--background-color);
-            color: var(--text-color);
-        }
-
-        .container {
-            max-width: 768px;
-            margin: 0 auto;
-            padding: 1rem;
-        }
-
-        h1 {
-            font-size: 26px;
-            font-weight: 500;
-        }
-
-        p {
-            font-size: 18px;
-        }
-
-        em {
-            color: var(--accent-color);
-            font-size: 15px;
-        }
-
-        .progress-bar {
-            height: 4px;
-            background-color: var(--progress-bg-color);
-            width: 100%;
-            overflow: hidden;
-        }
-
-        .progress-bar-value {
-            width: 100%;
-            height: 100%;
-            background-color: var(--progress-value-color);
-            animation: indeterminateAnimation 1s infinite linear;
-            transform-origin: 0 50%;
-        }
-
-        @keyframes indeterminateAnimation {
-            0% {
-                transform: translateX(0) scaleX(0);
-            }
-            40% {
-                transform: translateX(0) scaleX(0.4);
-            }
-            100% {
-                transform: translateX(100%) scaleX(0.5);
-            }
-        }
-
-        @media only screen and (max-width: 600px) {
-            h1 {
-                font-size: 18px;
-            }
-
-            p {
-                font-size: 14px;
-            }
-
-            em {
-                font-size: 10px;
-            }
-        }
-    </style>
+    </script>
 </head>
-<body>
-<div class="container">
-    <noscript>Your browser does not support JavaScript!</noscript>
-    <div>
-        <h1>Redirecting...</h1>
-        <p>You will be redirected to the payment gateway in a few seconds.</p>
-        <div class="progress-bar" role="progressbar" aria-valuetext="Redirecting...">
-            <div class="progress-bar-value"></div>
-        </div>
-        <div style="margin-top: 10px">
-            <em>Please do not refresh the page or click the "Back" or "Close" button of your browser.</em>
+<body class="bg-black font-sans antialiased">
+<div class="bg-gradient-to-b from-blue-700 to-black h-screen">
+    <div class="absolute top-0 w-full flex justify-content-center items-center h-screen">
+        <div class="flex flex-col gap-5 mx-auto max-w-7xl bg-black bg-opacity-20 px-32 py-24 rounded-2xl">
+            <h1 class="text-white text-2xl font-medium">Redirecting to <span class="font-['Ubuntu']">Pay<span class="text-yellow-500">Here</span></span></h1>
+            <p class="text-white">{{ __('You will be redirected to the payment gateway in a few seconds.') }}</p>
         </div>
     </div>
-    <form id="checkout-form" action="{{ $data['action'] }}" method="post">
-        @foreach ($data['customer'] as $key => $value)
-            <input type="hidden" name="{{ $key }}" value="{{ $value }}">
-        @endforeach
-        
-        @isset($data['recurring'])
-            <input type="hidden" name="recurrence" value="{{ $data['recurring']['recurrence'] }}">
-            <input type="hidden" name="duration" value="{{ $data['recurring']['duration'] }}">
-        @endisset
-        
-        @isset($data['platform'])
-            <input type="hidden" name="platform" value="{{ $data['platform'] }}">
-        @endisset
-        
-        @isset($data['startup_fee'])
-            <input type="hidden" name="startup_fee" value="{{ $data['startup_fee'] }}">
-        @endisset
-        
-        @isset($data['custom_1'])
-            <input type="hidden" name="custom_1" value="{{ $data['custom_1'] }}">
-        @endisset
-        
-        @isset($data['custom_2'])
-            <input type="hidden" name="custom_2" value="{{ $data['custom_2'] }}">
-        @endisset
-
-        @isset($data['title'])
-            <input type="hidden" name="items" value="{{ $data['title'] }}">
-        @endisset
-        
-        @foreach ($data['items'] as $key => $value)
-            <input type="hidden" name="{{ $key }}" value="{{ $value }}">
-        @endforeach
-
-        <input type="hidden" name="merchant_id" value="{{ $data['merchant_id'] }}">
-        <input type="hidden" name="notify_url" value="{{ $data['notify_url'] }}">
-        <input type="hidden" name="return_url" value="{{ $data['return_url'] }}">
-        <input type="hidden" name="cancel_url" value="{{ $data['cancel_url'] }}">
-        <input type="hidden" name="order_id" value="{{ $data['order_id'] }}">
-        <input type="hidden" name="currency" value="{{ $data['currency'] }}">
-        <input type="hidden" name="amount" value="{{ $data['amount'] }}">
-        <input type="hidden" name="hash" value="{{ $data['hash'] }}">
-    </form>
 </div>
-<script>
-    setTimeout(function () {
-        document.getElementById('checkout-form').submit();
-    }, 1000);
-</script>
 </body>
 </html>
