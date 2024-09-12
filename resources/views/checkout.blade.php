7a4b9156b8b1b902eade5c061592c318e1eb7d3b
Dasun Tharanga <hello@dasun.dev>
2024-09-12 16:11:01 +0530

    wip

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -5,43 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     
     <title>{{ __('Redirecting to PayHere') }}</title>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net" />
-    <link href="https://fonts.bunny.net/css?family=ubuntu:400,500,700" rel="stylesheet" />
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600" rel="stylesheet" />
-
-    <script src="https://cdn.tailwindcss.com"></script>
-    
-    <script>
-        tailwind.config = {
-            theme: {
-                fontFamily: {
-                    sans: ["Figtree", "sans-serif"],
-                },
-                extend: {
-                    colors: {
-                        blue: {
-                            700: "#2447d6",
-                        },
-                        yellow: {
-                            500: "#fcac00",
-                        },
-                        black: "#09090b",
-                    },
-                },
-            }
-        }
-    </script>
 </head>
-<body class="bg-black font-sans antialiased">
-<div class="bg-gradient-to-b from-blue-700 to-black h-screen">
-    <div class="absolute top-0 w-full flex justify-content-center items-center h-screen">
-        <div class="flex flex-col gap-5 mx-auto max-w-7xl bg-black bg-opacity-20 px-24 py-24 rounded-2xl">
-            <h1 class="text-white text-2xl font-medium">Redirecting to <span class="font-['Ubuntu']">Pay<span class="text-yellow-500">Here</span></span></h1>
-            <p class="text-white">{{ __('You will be redirected to the payment gateway in a few seconds.') }}</p>
-        </div>
-    </div>
-</div>
+<body>
+<p>Redirecting to PayHere...</p>
+<form id="checkoutForm" action="{{ $data['action'] }}" method="post">
+    @foreach ($data['customer'] as $key => $value)
+        <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+    @endforeach
+
+    @isset($data['recurring'])
+        <input type="hidden" name="recurrence" value="{{ $data['recurring']['recurrence'] }}">
+        <input type="hidden" name="duration" value="{{ $data['recurring']['duration'] }}">
+    @endisset
+
+    @isset($data['platform'])
+        <input type="hidden" name="platform" value="{{ $data['platform'] }}">
+    @endisset
+
+    @isset($data['startup_fee'])
+        <input type="hidden" name="startup_fee" value="{{ $data['startup_fee'] }}">
+    @endisset
+
+    @isset($data['custom_1'])
+        <input type="hidden" name="custom_1" value="{{ $data['custom_1'] }}">
+    @endisset
+
+    @isset($data['custom_2'])
+        <input type="hidden" name="custom_2" value="{{ $data['custom_2'] }}">
+    @endisset
+
+    @isset($data['title'])
+        <input type="hidden" name="items" value="{{ $data['title'] }}">
+    @endisset
+
+    @foreach ($data['items'] as $key => $value)
+        <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+    @endforeach
+
+    <input type="hidden" name="merchant_id" value="{{ $data['merchant_id'] }}">
+    <input type="hidden" name="notify_url" value="{{ $data['notify_url'] }}">
+    <input type="hidden" name="return_url" value="{{ $data['return_url'] }}">
+    <input type="hidden" name="cancel_url" value="{{ $data['cancel_url'] }}">
+    <input type="hidden" name="order_id" value="{{ $data['order_id'] }}">
+    <input type="hidden" name="currency" value="{{ $data['currency'] }}">
+    <input type="hidden" name="amount" value="{{ $data['amount'] }}">
+    <input type="hidden" name="hash" value="{{ $data['hash'] }}">
+</form>
+<script>
+    setTimeout(function () {
+        document.getElementById('checkoutForm').submit();
+    });
+</script>
 </body>
 </html>
