4af8476ae31bbe45f85a47087644429cf0a41555
Dasun Tharanga <hello@dasun.dev>
2024-09-12 15:43:05 +0530

    wip

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     
-    <title>{{ __('Checkout') }}</title>
+    <title>{{ __('Redirecting to PayHere') }}</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net" />
