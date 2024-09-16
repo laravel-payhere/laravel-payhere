3c1cf2c7f4b935fa03b95f278cb49cc71bafde9c
Dasun Tharanga <hello@dasun.dev>
2024-09-16 11:44:56 +0530

    wip

--- resources/views/return.blade.php
+++ resources/views/return.blade.php
@@ -23,7 +23,7 @@
                 </svg>
             </div>
             <div class="ml-6">
-                <h2 class="text-xl font-semibold text-gray-900">Payment successful.</h2>
+                <h2 class="text-xl font-semibold text-gray-900">Payment successful</h2>
 
                 <p class="mt-2 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                     Your payment is currently being verified.
