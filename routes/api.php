f5347bec77b09b431e14b81f8f525e53e4f5c55d
Dasun Tharanga <hello@dasun.dev>
2024-09-20 14:47:43 +0530

    wip

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ Route::group(['prefix' => 'payhere/api', 'as' => 'payhere.api.'], function () {
     });
     Route::prefix('subscriptions')->as('subscription.')->group(function () {
         Route::get('/', [SubscriptionController::class, 'index'])->name('index');
-        Route::get('{id}', [SubscriptionController::class, 'show'])->name('show');
+        Route::get('{id}/payments', [SubscriptionController::class, 'show'])->name('show');
         Route::post('{id}/retry', [SubscriptionController::class, 'retry'])->name('retry');
         Route::delete('{id}', [SubscriptionController::class, 'cancel'])->name('cancel');
     });
