a7601733a3cff706f374224dcfb25c50816816e0
Dasun Tharanga <hello@dasun.dev>
2024-09-20 22:39:58 +0530

    wip

--- tests/Browser/CheckoutTest.php
+++ tests/Browser/CheckoutTest.php
@@ -5,18 +5,15 @@ declare(strict_types=1);
 namespace PayHere\Tests\Browser;
 
 use Laravel\Dusk\Browser;
-use Orchestra\Testbench\Concerns\WithWorkbench;
 use PayHere\Tests\Browser\Pages\Checkout;
 use Workbench\App\Models\User;
 
-uses(WithWorkbench::class);
-
 it('can checkout', function () {
     $user = User::factory()->create();
-
+    
     $this->browse(function (Browser $browser) use ($user) {
         $browser->loginAs($user)
-            ->assertAuthenticatedAs($user);
+            ->assertAuthenticated();
 
         $browser->visit(new Checkout)
             ->payAs($user)
--- tests/Pest.php
+++ tests/Pest.php
@@ -24,7 +24,7 @@ uses(
 
 uses(
     DuskTestCase::class,
-    DatabaseTruncation::class
+    RefreshDatabase::class
 )->in('Browser');
 
 /*
