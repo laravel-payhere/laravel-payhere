caafb9840d2181a7bdbf3d996ba455b0cdf313c8
Dasun Tharanga <hello@dasun.dev>
2024-09-21 13:18:35 +0530

    wip

--- tests/Browser/CheckoutTest.php
+++ tests/Browser/CheckoutTest.php
@@ -7,6 +7,8 @@ namespace PayHere\Tests\Browser;
 use Laravel\Dusk\Browser;
 use PayHere\Tests\Browser\Pages\Authorize;
 use PayHere\Tests\Browser\Pages\Checkout;
+use PayHere\Tests\Browser\Pages\Preapproval;
+use PayHere\Tests\Browser\Pages\Recurring;
 use PayHere\Tests\DuskTestCase;
 use Workbench\App\Models\User;
 
@@ -37,6 +39,38 @@ class CheckoutTest extends DuskTestCase
                 ->assertAuthenticatedAs($user);
 
             $browser->visit(new Authorize)
+                ->payAs($user)
+                ->assertPaymentApproved();
+        });
+
+        $this->assertDatabaseCount('payhere_payments', 1);
+    }
+
+    public function test_preapprove_checkout()
+    {
+        $user = User::factory()->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                ->assertAuthenticatedAs($user);
+
+            $browser->visit(new Preapproval)
+                ->payAs($user)
+                ->assertPaymentApproved();
+        });
+
+        $this->assertDatabaseCount('payhere_payments', 1);
+    }
+
+    public function test_recurring_checkout()
+    {
+        $user = User::factory()->create();
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->loginAs($user)
+                ->assertAuthenticatedAs($user);
+
+            $browser->visit(new Recurring)
                 ->payAs($user)
                 ->assertPaymentApproved();
         });
