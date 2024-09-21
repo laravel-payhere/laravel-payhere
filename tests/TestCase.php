0ee9bb9ef2ca921a853d84c0a53caed48f31ccbb
Dasun Tharanga <hello@dasun.dev>
2024-09-21 11:59:26 +0530

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace PayHere\Tests;
 
-use Orchestra\Testbench\Attributes\WithMigration;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use PayHere\PayHereServiceProvider;
 use Workbench\App\Providers\WorkbenchServiceProvider;
 
-#[WithMigration]
 abstract class TestCase extends BaseTestCase
 {
     use WithWorkbench;
