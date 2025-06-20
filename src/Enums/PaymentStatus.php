20e6d6f91ec48c345d3bc4dd84565d95984c507f
Dasun Tharanga <hello@dasun.dev>
2025-06-20 23:01:24 +0530

    wip

--- src/Enums/PaymentStatus.php
+++ src/Enums/PaymentStatus.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace PayHere\Enums;
 
-use Filament\Support\Contracts\HasColor;
-use Filament\Support\Contracts\HasLabel;
-
-enum PaymentStatus: int implements HasColor, HasLabel
+enum PaymentStatus: int
 {
     case AuthorizationSuccess = 3;
     case Success = 2;
@@ -15,27 +12,4 @@ enum PaymentStatus: int implements HasColor, HasLabel
     case Cancelled = -1;
     case Failed = -2;
     case Chargeback = -3;
-
-    public function getLabel(): ?string
-    {
-        return match ($this) {
-            self::AuthorizationSuccess => 'Authorization Success',
-            self::Success => 'Success',
-            self::Pending => 'Pending',
-            self::Cancelled => 'Cancelled',
-            self::Failed => 'Failed',
-            self::Chargeback => 'Chargeback'
-        };
-    }
-
-    public function getColor(): string|array|null
-    {
-        return match ($this) {
-            self::AuthorizationSuccess, self::Success => 'success',
-            self::Pending => 'warning',
-            self::Cancelled => 'gray',
-            self::Failed => 'danger',
-            self::Chargeback => 'info'
-        };
-    }
 }
